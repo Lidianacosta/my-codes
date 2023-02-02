8b327fa99e8fbf4e5942e57ea6bc3672a6c8cbc4
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-02 13:54:14 -0300

    Add files via upload

---
+++ alocacaolabq1.c
@@ -0,0 +1,24 @@
+// 1. Aloque dinamicamente um vetor de inteiros de tamanho 10 e preencha cada elemento do vetor com
+// o seu respectivo ´ındice (vetor[i] = i). Em seguida, imprima o vetor.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 10
+
+int main (void){
+
+    int cont;
+    int *v = (int*)malloc(N*sizeof(int));
+
+    for (cont = 0; cont < N; cont++)
+    {
+        *(v + cont) = cont;
+        printf("%d\t",*(v + cont));
+    }
+
+
+
+free(v);
+    return(0);
+}
