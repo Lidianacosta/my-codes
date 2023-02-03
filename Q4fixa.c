660e3be3590ec3db954792efd4c9f9ae19243fa1
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-03 12:02:00 -0300

    Update Q4fixa.c

--- Q4fixa.c
+++ Q4fixa.c
@@ -1,3 +1,13 @@
+// Questão 4) Crie um programa para calcular a área e o perímetro de um hexágono. O seu
+// programa deve implementar uma função chamada calcula_hexagono que calcula a área e o
+// perímetro de um hexágono regular de lado l. A função deve obedecer ao seguinte protótipo:
+// void calcula_hexagono(float l, float *area, float *perimetro); A área e o perímetro de um
+// hexágono regular são dados, respectivamente, por:
+//            A = (3*l*l*sqrt(3))/2
+//            P =6*l        
+//
+
+
 #include<stdio.h>
 #include<math.h>
 
