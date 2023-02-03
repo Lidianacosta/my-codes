48b1d8ba9391572fa8499f0d4da03d800194f7ac
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-03 12:11:01 -0300

    Create distacia entre dois pontos ponteiros.c

---
+++ EU/distacia entre dois pontos ponteiros.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+void calcular_distancia(float *distancia, float x1,float y1 , float x2, float y2){
+
+*distancia = sqrt(pow(y2 - y1,2)+ pow(x2 - x1,2));
+
+}
+
+int main(){
+
+ float x1,x2,y1,y2,distancia;
+
+x1 = x2 = y1 = y2 = 10;
+
+
+calcular_distancia(&distancia,9.0,3.0,2.0,8.0);
+
+
+printf("%.1f",distancia);
+
+    return(0);
+}
+
