fc8923a5a4fabe236ff93c46e6343bfb132d07d6
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-07 19:49:44 -0300

    Update tentando fazer o mapa.c

--- EU/tentando fazer o mapa.c
+++ EU/tentando fazer o mapa.c
@@ -23,118 +23,18 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
-//===================================================================================================================
-//                                tava so testando
-
-void pedir_o_mapa(int m,int n, char **matriz){
-
-int linha ,coluna;
-printf("mostre como e seu mapa digite '.' para mar e '#'para terra\n");
-for(linha = 0; linha<m ;linha++){
-
-scanf(" %[^\n]s",matriz[linha]);
-
-}
-
-printf("sua matriz ta aqui\n");
-for(linha = 0; linha<m ;linha++){
-
-
-printf("%s\n",matriz[linha]);
-
-
-}
-}
-
-//================================================================================================================
-
-void verifica_costa(int m ,int n,char **matriz){
-
-int linha ,coluna,soma_costas = 0;
-
-for(linha = 0;linha<m;linha++){
-    for(coluna = 0;coluna<n;coluna++){
-//=================================================================================================================================================================================
-        if(matriz[linha][coluna] == '#'){
-
-            if (linha != (m-1) && linha != 0 && coluna != (n-1) && coluna != 0){
                 
-                if (matriz[linha][coluna +1] == '.' || matriz[linha][coluna -1] == '.' || matriz[linha + 1][coluna] == '.' || matriz[linha - 1][coluna] == '.'){
-                soma_costas++;
-                }
-            
-//=================================================================================================================================================================================
-        // verificando a borda 'linha' (m -1) de baixo e coluna == 0 e coluna == (n-1);
-        }else if (linha == (m-1)){
-            if(coluna != 0 && coluna != (n-1)){
-            
-                 //   if ( matriz[linha][coluna +1] == '.' && matriz[linha ][coluna -1] == '.' && matriz[linha -1][coluna ]){
+void pedir_o_mapa(int m,int n, char **matriz);
 
-                        soma_costas++;
-                //    }
+void verifica_costa(int m ,int n,char **matriz);
 
-            }else if(coluna == 0){
+int main(void){
 
-                  //  if ( matriz[linha][coluna +1] == '.'  && matriz[linha -1][coluna]){
-
-                     soma_costas++;
-
-                  //  }
-
-            }else if(coluna == (n-1)){
-
-                 //   if ( matriz[linha][coluna -1] == '.'  && matriz[linha -1][coluna]){
-
-                     soma_costas++;
-
-                  //  }
-             }
-
- //==================================================================================================================================================================================           
-            // verificando linha 0
-        }else if (linha == 0){
-              
-            if(coluna != (n-1) && coluna != 0){
-              
-              //  if (matriz[linha][coluna +1] == '.' ||  matriz[linha][coluna -1] == '.' || matriz[linha + 1][coluna ] == '.'){
-                soma_costas++;
-              //  }
-            
-            }else if(coluna == 0){
-
-              //  if (matriz[linha][coluna +1] == '.' ||  matriz[linha + 1][coluna] == '.' ){
-                soma_costas++;
-             //   }
-
-            }else if(coluna == (n-1)){
-
-             //   if (matriz[linha][coluna -1] == '.' ||  matriz[linha + 1][coluna] == '.' ){
-                soma_costas++;
-              //  }
-            }     
-//======================================================================================================================================================================================
-        }
-
-      }
-    }
-}
-
-
-printf("a quantidade de costas e : ");
-printf(" %d",soma_costas);
-
-}
-
-int main(void)
-{
-
-//============================================
     int m,n,cont;
     printf("digite o numero de linhas e colunas\n");
    
     scanf("%d %d",&m,&n);
-//============================================
+
     char **mapa = (char**)malloc(m*sizeof(char*));
 
     if(mapa == NULL) {
@@ -143,32 +43,63 @@ int main(void)
     }
 
     for(cont = 0; cont < m; cont++) {
-
-        mapa[cont] = (char*)malloc(n*sizeof(char));
-          
-          if(mapa[cont] == NULL) {
+        mapa[cont] = (char*)malloc(n*sizeof(char));     
+        if(mapa[cont] == NULL) {
         printf("erro");
         exit(1);
+        }
     }
-
-    }
-//============================================
 
     pedir_o_mapa(m,n,mapa);
     verifica_costa(m,n,mapa);
 
-
-//============================================
     for(cont = 0; cont < m; cont++) {
-
         free(mapa[cont]);
-
     }
 
-
  free(mapa);
-
-
-
     return(0);
+}
+
+void pedir_o_mapa(int m,int n, char **matriz){
+ int linha ,coluna;
+    printf("mostre como e seu mapa digite '.' para mar e '#'para terra\n");
+    for(linha = 0; linha<m ;linha++){
+    scanf(" %[^\n]s",matriz[linha]);
+    }
+}
+
+void verifica_costa(int m ,int n,char **matriz){
+
+  int linha ,coluna,soma_costas = 0;
+
+    for(linha = 0;linha<m;linha++){
+         for(coluna = 0;coluna<n;coluna++){
+           if(matriz[linha][coluna] == '#'){
+                if (linha != (m-1) && linha != 0 && coluna != (n-1) && coluna != 0){
+                    if (matriz[linha][coluna +1] == '.' || matriz[linha][coluna -1] == '.' || matriz[linha + 1][coluna] == '.' || matriz[linha - 1][coluna] == '.'){
+                        soma_costas++;
+                    }
+                }else if (linha == (m-1)){
+                    if(coluna != 0 && coluna != (n-1)){
+                        soma_costas++;
+                    }else if(coluna == 0){
+                        soma_costas++;
+                    }else if(coluna == (n-1)){
+                        soma_costas++; 
+                    }
+                }else if (linha == 0){  
+                    if(coluna != (n-1) && coluna != 0){
+                        soma_costas++;
+                    }else if(coluna == 0){
+                        soma_costas++;
+                    }else if(coluna == (n-1)){
+                        soma_costas++;
+                    }     
+                }
+            }
+        }
+    }
+    printf("a quantidade de costas e : ");
+    printf(" %d",soma_costas);
 }
