cf6e9cfb9de88b61703222b5e8e5969158ef27c0
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-08 16:42:13 -0300

    Create dados_funcionario.c

---
+++ estrutura I/dados_funcionario.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct dados_funcionario{
+    char nome_fucionario[50];
+    float salario;
+    char cargo[50];
+}Dados_funcionario;
+
+void cadastra(int n, Dados_funcionario *f);
+
+int main(void){
+    int n,count;
+    printf("digite o numero de funcionarios que deseja cadastra: ");
+    scanf("%d",&n);
+
+    Dados_funcionario *funcionarios = (Dados_funcionario*)malloc(n * sizeof(Dados_funcionario));
+
+    cadastra(n,funcionarios);
+
+    for(count = 0 ; count < n ; count++){
+        printf("%s\n",funcionarios->nome_fucionario);
+        printf("%f\n",funcionarios->salario);
+        printf("%s\n",funcionarios->cargo);
+    }
+
+    free(funcionarios);
+    return(0);
+}
+
+ void cadastra(int n, Dados_funcionario *f){
+     int count;
+     for(count = 0 ; count < n ; count++){
+        printf("digite o nome do funcionario\n");
+        scanf(" %[^\n]s",f->nome_fucionario);
+        printf("digite o quanto o funcionario recebe\n");
+        scanf("%f",&f->salario);
+        printf("digite o qual o cargo do funcionario\n");
+        scanf(" %[^\n]s",f->cargo);
+     }
+ }
