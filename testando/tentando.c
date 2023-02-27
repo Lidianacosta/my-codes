652cb1f7f627d564e509c44d3a9bd5c3e1356054
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-27 16:25:03 -0300

    Create tentando.c

---
+++ testando/tentando.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+// void troca (int *p){
+//     *p = 12;
+// }
+// void main(void){
+// int n;
+// printf("digite um numero: ");
+// scanf("%d",&n);
+// troca(&n);
+// printf("o numero foi modificado : %d",n);
+// }
+
+
+// void imprime(int *v,int n){
+//     int i;
+//     for(i=0;i<n;i++){
+//         printf("v[%d] = %p\t",i,v+1);
+//     } 
+//     }
+//         void prencher(int *v,int n){
+//         int i;
+//         for(i=0;i<n;i++){
+//             v[i] = i;
+//         } 
+//         }
+
+// void main(void){
+// int n;
+// printf("digite o tamanho do vetor: ");
+// scanf("%d",&n);
+// int v[n];
+
+// prencher(v,n);
+// imprime(v,n);
+
+// }
