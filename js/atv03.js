c1ed6ac5c9c1859045d308529bff880ab829b515
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-07 15:08:28 -0300

    Create atv03.js

---
+++ js/atv03.js
@@ -0,0 +1,11 @@
+let n = prompt("digite um numero: ");
+
+
+console.log(Math.pow(n,3));
+let n2 = prompt("digite um numero: ");
+console.log(Math.sqrt(n2));
+
+const vetor  = [9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 1]
+
+console.log(Math.max(...vetor));
+console.log(Math.min(...vetor)); 	
