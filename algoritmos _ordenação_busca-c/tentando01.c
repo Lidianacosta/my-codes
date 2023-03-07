6e8fa4d1ef2be78543a7dff7198ff8d67137e9aa
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-07 14:39:35 -0300

    Update tentando01.c

--- algoritmos _ordenação_busca-c/tentando01.c
+++ algoritmos _ordenação_busca-c/tentando01.c
@@ -96,3 +96,72 @@ int busca_interpolada(int *vet ,int tam, int idx){
     }
 
 }
+
+	#include <stdio.h>
+2	
+3	#define TAM 10
+4	
+5	void selecao(int* vet ,int tam);
+6	int busca_interpolada(int *vet ,int tam, int idx);
+7	
+8	int main(void){
+9	    int i,index,idx,tam;
+10	    int vetor[TAM] = {8,6,4,7,9,1,3,5,0,2};
+11	    idx = 9;
+12	    tam = sizeof(vetor)/sizeof(vetor[0]);
+13	    selecao(vetor,TAM);
+14	   
+15	    index = busca_interpolada(vetor,tam,idx);
+16	
+17	    if(index != -1){
+18	        printf("elemento encontrado no index %d", index);
+19	    }else{
+20	        printf("elemento nao encontrado no vetor!");
+21	    }
+22	    
+23	    return(0);
+24	}
+25	void selecao(int* vet ,int tam){
+26	    int i,j,min,x;
+27	    for(i = 0 ; i < tam-1 ; i++){
+28	        min = i;
+29	        for (j = i+1; j < tam ; j++){
+30	            if (vet[j] < vet[min]){
+31	                min = j;
+32	            }
+33	        }
+34	        x = vet[min];
+35	        vet[min] = vet[i];
+36	        vet[i] = x ;
+37	    }
+38	
+39	    for (i = 0; i < TAM; i++){
+40	        printf("v[%d] = %d\n",i,vet[i]);
+41	    }
+42	    printf("ordenado com Selection Sort!");
+43	}
+44	int busca_interpolada(int *vet ,int tam, int idx){
+45	    if(tam == 0){
+46	        return(-1);
+47	    }
+48	    int menor = 0, maior = tam - 1, meio;
+49	
+50	    while(vet[maior] != vet[menor] && idx >= vet[menor] && idx<=vet[maior]){
+51	        meio = menor + ((idx - vet[menor]) * (maior - menor)/(vet[maior] - vet[menor]));
+52	    if(idx == vet[meio]){
+53	        return(meio);
+54	    }
+55	    else if (idx <vet[meio]){
+56	        maior = meio -1;
+57	    }else{
+58	        menor = meio +1;
+59	    }
+60	    }
+61	
+62	    if(idx == vet[menor]){
+63	        return(menor);
+64	    }else{
+65	        return(-1);
+66	    }
+67	
+68	}
